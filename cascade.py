859a99f8a41baa4cca74f436579ac96d62acf79a
Chase Duncan <cddunca2@frankenstein.bioen.illinois.edu>
2020-07-08 16:01:20 -0500

    Built CascadeNet; committing before integrating old pipe.

---
+++ cascade.py
@@ -0,0 +1,202 @@
+import torch
+import torch.nn as nn
+
+from model_utils import *
+
+
+class CoarseEncoder(nn.Module):
+    def __init__(self, input_channels=4):
+        super(Encoder, self).__init__()
+        self.initLayer = nn.Conv3d(input_channels, 16, 
+                kernel_size=3, stride=1, padding=1)
+        self.dropout = nn.Dropout3d()
+        self.block0 = ResNetBlock(16)
+        self.block1 = ResNetBlock(16)
+        self.ds1 = Downsample(16)
+        self.block2 = ResNetBlock(32)
+        self.block3 = ResNetBlock(32)
+        self.ds2 = Downsample(32)
+        self.block4 = ResNetBlock(64) 
+        self.block5 = ResNetBlock(64) 
+        self.ds3 = Downsample(64)
+        self.block6 = ResNetBlock(128) 
+        self.block7 = ResNetBlock(128) 
+        self.block8 = ResNetBlock(128) 
+        self.block9 = ResNetBlock(128)
+
+    def forward(self, x):
+        # sp* is the state of the output at each spatial level
+        sp0 = self.dropout(self.initLayer(x))
+        sp1 = self.block0(sp0)
+        sp1 = self.block1(sp0)
+        sp2 = self.ds1(sp1)
+        sp2 = self.block2(sp2)
+        sp2 = self.block3(sp2)
+        sp3 = self.ds2(sp2)
+        sp3 = self.block4(sp3)
+        sp3 = self.block5(sp3)
+        sp4 = self.ds3(sp3)
+        sp4 = self.block6(sp4)
+        sp4 = self.block7(sp4)
+        sp4 = self.block8(sp4)
+        sp4 = self.block9(sp4)
+
+        return {
+                'spatial_level_4':sp4, 'spatial_level_3':sp3, 
+                'spatial_level_2':sp2, 'spatial_level_1':sp1
+                }
+
+
+class CoarseDecoder(nn.Module):
+    def __init__(self, output_channels=3, bilinear=False):
+        super(Decoder, self).__init__()
+        self.cf1 = CompressFeatures(128, 64)
+        self.block9 = ResNetBlock(64) 
+        self.cf2 = CompressFeatures(64, 32)
+        self.block11 = ResNetBlock(32) 
+        self.cf3 = CompressFeatures(32, 16)
+        self.block13 = ResNetBlock(16)
+        self.cf_final = CompressFeatures(16, output_channels)
+        
+        if bilinear:
+            self.up1 = self.up2 = self.up3 =\
+                    nn.Upsample(scale_factor=2, mode='nearest')
+        else:
+            self.up1 = UpsamplingDeconv3d()
+            self.up2 = UpsamplingDeconv3d()
+            self.up3 = UpsamplingDeconv3d()
+        self.sig = nn.Sigmoid()
+
+    def forward(self, x):
+        sp3 = x['spatial_level_3'] + self.up1(self.cf1(x['spatial_level_4']))
+        sp3 = self.block9(sp3)
+        sp2 = x['spatial_level_2'] + self.up2(self.cf2(sp3))
+        sp2 = self.block11(sp2)
+        sp1 = x['spatial_level_1'] + self.up3(self.cf3(sp2))
+        sp1 = self.block13(sp1)
+        output = self.sig(self.cf_final(sp1))
+
+        return output
+     
+
+class Encoder(nn.Module):
+    def __init__(self, input_channels=4):
+        super(Encoder, self).__init__()
+        self.initLayer = nn.Conv3d(input_channels, 32, 
+                kernel_size=3, stride=1, padding=1)
+
+        self.dropout = nn.Dropout3d()
+        self.block0 = ResNetBlock(32)
+        self.block1 = ResNetBlock(32)
+        self.ds1 = Downsample(32)
+        self.block2 = ResNetBlock(64)
+        self.block3 = ResNetBlock(32)
+        self.ds2 = Downsample(64)
+        self.block4 = ResNetBlock(128) 
+        self.block5 = ResNetBlock(32)
+        self.ds3 = Downsample(128)
+        self.block6 = ResNetBlock(256) 
+        self.block7 = ResNetBlock(256) 
+        self.block8 = ResNetBlock(256) 
+        self.block9 = ResNetBlock(256)
+
+    def forward(self, x):
+        # sp* is the state of the output at each spatial level
+        sp0 = self.dropout(self.initLayer(x))
+        sp1 = self.block0(sp0)
+        sp1 = self.block1(sp0)
+        sp2 = self.ds1(sp1)
+        sp2 = self.block2(sp2)
+        sp2 = self.block3(sp2)
+        sp3 = self.ds2(sp2)
+        sp3 = self.block4(sp3)
+        sp3 = self.block5(sp3)
+        sp4 = self.ds3(sp3)
+        sp4 = self.block6(sp4)
+        sp4 = self.block7(sp4)
+        sp4 = self.block8(sp4)
+        sp4 = self.block9(sp4)
+
+        return {
+                'spatial_level_4':sp4, 'spatial_level_3':sp3, 
+                'spatial_level_2':sp2, 'spatial_level_1':sp1
+                }
+
+
+class BilineDecoder(nn.Module):
+    def __init__(self, output_channels=3):
+        super(Decoder, self).__init__()
+        self.cf1 = CompressFeatures(256, 128)
+        self.block9 = ResNetBlock(128) 
+        self.cf2 = CompressFeatures(128, 64)
+        self.block11 = ResNetBlock(64) 
+        self.cf3 = CompressFeatures(64, 32)
+        self.block13 = ResNetBlock(32)
+        self.cf_final = CompressFeatures(32, output_channels)
+
+        self.up = nn.Upsample(scale_factor=2, mode='nearest')
+        self.sig = nn.Sigmoid()
+
+    def forward(self, x):
+        sp3 = x['spatial_level_3'] + self.up(self.cf1(x['spatial_level_4']))
+        sp3 = self.block9(sp3)
+        sp2 = x['spatial_level_2'] + self.up(self.cf2(sp3))
+        sp2 = self.block11(sp2)
+        sp1 = x['spatial_level_1'] + self.up(self.cf3(sp2))
+        sp1 = self.block13(sp1)
+        output = self.sig(self.cf_final(sp1))
+
+        return output
+
+
+class DeconvDecoder(nn.Module):
+    def __init__(self, output_channels=3):
+        super(Decoder, self).__init__()
+        self.cf1 = CompressFeatures(256, 128)
+        self.block9 = ResNetBlock(128) 
+        self.cf2 = CompressFeatures(128, 64)
+        self.block11 = ResNetBlock(64) 
+        self.cf3 = CompressFeatures(64, 32)
+        self.block13 = ResNetBlock(32)
+        self.cf_final = CompressFeatures(32, output_channels)
+
+        self.up43 = UpsamplingDeconv3d()
+        self.up32 = UpsamplingDeconv3d()
+        self.up21 = UpsamplingDeconv3d()
+
+        self.up = nn.Upsample(scale_factor=2, mode='nearest')
+        self.sig = nn.Sigmoid()
+
+    def forward(self, x):
+        sp3 = x['spatial_level_3'] + self.up43(self.cf1(x['spatial_level_4']))
+        sp3 = self.block9(sp3)
+        sp2 = x['spatial_level_2'] + self.up32(self.cf2(sp3))
+        sp2 = self.block11(sp2)
+        sp1 = x['spatial_level_1'] + self.up21(self.cf3(sp2))
+        sp1 = self.block13(sp1)
+        output = self.sig(self.cf_final(sp1))
+
+        return output
+     
+
+class CascadeNet(nn.Module):
+    def __init__(self):
+        super(LeaNet, self).__init__()
+        self.coarse_encoder = CoarseEncoder()
+        self.coarse_decoder = CoarseDecoder()
+        self.encoder = Encoder()
+        self.deconv_decoder = DeconvDecoder()
+        self.biline_decoder = BilineDecoder()
+
+    def forward(self, x):
+        x = self.coarse_encoder(x)
+        coarse= self.coarse_decoder(x)
+        x = self.encoder(coarse_x)
+        deconv = self.deconv_decoder(x)
+        biline = self.biline_decoder(x)
+        return  { 
+                'coarse': coarse, 
+                'biline': biline, 
+                'deconv': deconv 
+                }
+
--- losses.py
+++ losses.py
@@ -98,6 +98,18 @@ class AvgDiceLoss(nn.Module):
     avg_dice = torch.einsum('c->', proportions) / (target.shape[0]*target.shape[1])
     return 1 - avg_dice
 
+class CascadeAvgDiceLoss(nn.Module):
+    def __init__(self):
+        super(CascadeAvgDiceLoss, self).__init__()
+        self.coarse_loss = AvgDiceLoss()
+        self.deconv_loss = AvgDiceLoss()
+        self.biline_loss = AvgDiceLoss()
+
+    def forward(self, output, targets):
+        return 0.33*(self.coarse_loss(output['coarse'], targets['target'])\
+                + self.biline_loss(output['biline'], targets['target'])\
+                + self.deconv_loss(output['deconv'], targets['target']))
+
 class DiceLoss(nn.Module):
   def __init__(self):
     super(DiceLoss, self).__init__()

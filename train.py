VAEReg is back in. The latent variance goes to infinity so something is wrong. Not sure if it's the class, loss or something else.
Added some QoL stuff to train.py and utils.py.

train.py
@@ -21,6 +21,7 @@ from utils import (
     )
 
 from models.cascade_net import CascadeNet
+from models.vaereg import VAEReg
 from torch.utils.data import DataLoader
 from scheduler import PolynomialLR
 import losses
@@ -59,6 +60,9 @@ parser.add_argument('-a', '--augment_data', action='store_true',
 
 parser.add_argument('--mixed_precision', action='store_true', 
     help='mixed precision flag (default: off)')
+
+parser.add_argument('-b', '--debug', action='store_true', 
+    help='use debug mode which only uses a couple examples for training and testing (default: off)')
 
 parser.add_argument('-L', '--large_patch', action='store_true', 
         help='use patch size 160x192x128 (default patch size: 128x128x128)')
@@ -190,6 +194,9 @@ if args.model == 'MonoUNet':
 if args.model == 'CascadeNet':
     model = CascadeNet()
     loss = losses.CascadeAvgDiceLoss()
+if args.model == 'VAEReg':
+    model = VAEReg()
+    loss = losses.VAEDiceLoss()
 
 optimizer = \
     optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.wd)
@@ -213,6 +220,7 @@ writer = SummaryWriter(log_dir=f'{args.dir}/logs')
 scheduler = None
 
 swa_start = 1
+opt = None
 if args.swa:
     optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
     opt = SWA(optimizer, swa_start=swa_start, swa_freq=1, swa_lr=0.05)
@@ -242,20 +250,23 @@ for epoch in range(start_epoch, args.epochs):
                 opt, 
                 trainloader, 
                 device, 
-                mixed_precision=args.mixed_precision)
+                mixed_precision=args.mixed_precision,
+                debug=args.debug)
     else:
          train(model, 
                 loss, 
                 optimizer, 
                 trainloader, 
                 device, 
-                mixed_precision=args.mixed_precision)
+                mixed_precision=args.mixed_precision,
+                debug=args.debug)
        
     if args.swa and epoch > swa_start:
         opt.swap_swa_sgd()
 
     if (epoch + 1) % args.save_freq == 0:
-        opt.bn_update(trainloader, model)
+        if args.swa:
+            opt.bn_update(trainloader, model)
         save_checkpoint(
                 f'{args.dir}/checkpoints',
                 epoch + 1,
@@ -265,7 +276,8 @@ for epoch in range(start_epoch, args.epochs):
                 )
     
     if (epoch + 1) % args.eval_freq == 0:
-        opt.bn_update(trainloader, model)
+        if args.swa:
+            opt.bn_update(trainloader, model)
         # Evaluate on training data
         #train_val = validate(model, loss, trainloader, device)
         #time_ep = time.time() - time_ep
@@ -281,7 +293,7 @@ for epoch in range(start_epoch, args.epochs):
         #        columns, tablefmt="simple", floatfmt="8.4f")
         #print(table)
         model.eval()
-        eval_val = validate(model, loss, valloader, device)
+        eval_val = validate(model, loss, valloader, device, debug=args.debug)
 
         writer.add_scalar(f'{args.dir}/logs/loss/eval', eval_val['loss'], epoch)
         et, wt, tc = eval_val['dice']
@@ -295,7 +307,7 @@ for epoch in range(start_epoch, args.epochs):
         time_ep = time.time() - time_ep
         memory_usage = torch.cuda.memory_allocated() / (1024.0 ** 3)
 
-        eval_values = ['eval', epoch + 0, eval_val['loss'].data] \
+        eval_values = ['eval', epoch + 1, eval_val['loss'].data] \
           + eval_val['dice'].tolist()\
           + [ time_ep, memory_usage] 
 

utils.py
@@ -17,7 +17,8 @@ import torch.utils.data.sampler as sampler
 from tqdm import tqdm
 from models import (
         models,
-        cascade_net
+        cascade_net,
+        vaereg
         )
 #from apex import amp
 from apex_dummy import amp
@@ -175,10 +176,12 @@ def process_segs(seg):
         seg_et[np.where(seg[b, :, :, :] == 3)] = 1
         seg_t.append(seg_et)
         segs.append(seg_t)
+
     return torch.from_numpy(np.array(segs))
 
 # all the training and validation functions need to get out of here
-def train(model, loss, optimizer, train_dataloader, device, mixed_precision=False):
+def train(model, loss, optimizer, train_dataloader, device, mixed_precision=False, 
+        debug=False):
     total_loss = 0
     model.train()
     for src, target in tqdm(train_dataloader):
@@ -188,6 +191,7 @@ def train(model, loss, optimizer, train_dataloader, device, mixed_precision=Fals
         output = model(src)
         cur_loss = loss(output, {'target':target, 'src':src})
         total_loss += cur_loss
+        #print(f'total_loss {total_loss}')
         cur_loss.backward()
         optimizer.step()
         if debug:
@@ -198,8 +202,8 @@ def train(model, loss, optimizer, train_dataloader, device, mixed_precision=Fals
         #else:
         #    cur_loss.backward()
         #    optimizer.step()
-   
-def _validate(model, loss, dataloader, device):
+ 
+def validate(model, loss, dataloader, device, debug=False):
     loss_total = 0
     dice_total = 0
     examples_total = 0
@@ -212,10 +216,11 @@ def _validate(model, loss, dataloader, device):
             src, target = src.to(device, dtype=torch.float),\
                 target.to(device, dtype=torch.float)
             output = model(src)
-
             loss_total += loss(output, {'target':target, 'src':src}) 
             if isinstance(model, models.MonoUNet): 
                 dice_total += dice_score(output, target)
+                #print(f'dice_total: {dice_total}')
+                dice_total += dice_score(output['seg_map'], target)
 
             ####### 
             # CascadeNet
@@ -225,17 +230,9 @@ def _validate(model, loss, dataloader, device):
             if debug:
               break
         avg_dice = dice_total / examples_total
-        # still need to work this math out
-        #avg_loss = loss_total / examples_total 
         avg_loss = loss_total / len(dataloader)
-    return avg_dice, avg_loss
-        
-
-def validate(model, loss, data_loader, device):
-    dice_avg, loss_avg =\
-        _validate(model, loss, data_loader, device)
     
-    return {'dice':dice_avg, 
-            'loss':loss_avg
+    return {'dice':avg_dice, 
+            'loss':avg_loss
             }
 